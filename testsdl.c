c4664097d5e31099284bf8b0e72ed0bc1583f221
DamiBoul <damien@Ubuntu.myguest.virtualbox.org>
2023-06-05 16:40:43 +0200

    enfin j'ai réussi

--- testsdl.c
+++ testsdl.c
@@ -13,12 +13,6 @@ void TestAffichage(){
     SDL_Event event;
     SDL_bool program_launched = 1;
 
-    //Initialisation de SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0){
-        printf("Echec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
     //Création de la fenêtre
     pWindow = SDL_CreateWindow("Test d'affichage Pikachu",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, 720, 720, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
 
@@ -142,25 +136,20 @@ void TestAffichage(){
         }
     }
 
-    SDL_Quit();
 }
 
 void TestMatrice(){
 
     //Déclaration de variables
-    char nom_fichier[60] = "src/img/encore.bmp";
+    char nom_fichier[60] = "src/img/encore.png";
     SDL_Surface *imageSurface;
     Uint32 *pixels;
-    Uint32 pixel;
+    //Uint32 pixel;
     int width, height;
-    Uint32 **matrix;
-
-    //Initialisations
-    SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_PNG);
+    Uint8 **rmatrix, **gmatrix, **bmatrix, **amatrix;
 
     //Chargement de l'image
-    imageSurface = SDL_LoadBMP(nom_fichier);
+    imageSurface = IMG_Load(nom_fichier);
 
     //Mise en lecture de pixel
     SDL_LockSurface(imageSurface);
@@ -168,6 +157,37 @@ void TestMatrice(){
     width = imageSurface->w;
     height = imageSurface->h;
 
+    //Initialisation des matrices
+    rmatrix = malloc(height * width * sizeof(Uint8*));
+    gmatrix = malloc(height * width * sizeof(Uint8*));
+    bmatrix = malloc(height * width * sizeof(Uint8*));
+    amatrix = malloc(height * width * sizeof(Uint8*));
+
+    //Attribution des pixels aux matrices
+    int i;
+    for(i=0;i<width*height;i++){
+        SDL_GetRGBA(pixels[i], imageSurface->format, rmatrix[i], gmatrix[i], bmatrix[i], amatrix[i]);
+    }
+
+    //Affichage des matrices
+    for(i=0;i<width*height;i++){
+        printf("Pixel Color -> R: %d,  G: %d,  B: %d,  A: %d\n", *(rmatrix[i]), *(gmatrix[i]), *(bmatrix[i]), *(amatrix[i]));
+    }
+
+    //Nettoyage memoire
+    for(i=0;i<height*width;i++){
+        free(rmatrix[i]);
+        free(gmatrix[i]);
+        free(bmatrix[i]);
+        free(amatrix[i]);
+    }
+    free(rmatrix);
+    free(gmatrix);
+    free(bmatrix);
+    free(amatrix);
+
+//Ancienne méthode
+/*
     //Initialisation de la matrice
     matrix = malloc(height * sizeof(Uint32*));
     
@@ -192,26 +212,216 @@ void TestMatrice(){
         free(matrix[i]);
     }
     free(matrix);
+*/
     SDL_UnlockSurface(imageSurface);
     SDL_FreeSurface(imageSurface);
-    IMG_Quit();
-    SDL_Quit();
+
+
+}
+
+
+SDL_Color GetPixelColor(const SDL_Surface* pSurface, const int X, const int Y){
+
+    // Bytes per pixel
+    const Uint8 Bpp = pSurface->format->BytesPerPixel;
+
+    /*
+    Retrieve the address to a specific pixel
+    pSurface->pixels	= an array containing the SDL_Surface' pixels
+    pSurface->pitch		= the length of a row of pixels (in bytes)
+    X and Y				= the offset on where on the image to retrieve the pixel, (0, 0) is in the upper left corner of the image
+    */
+    Uint8* pPixel = (Uint8*)pSurface->pixels + Y * pSurface->pitch + X * Bpp;
+
+    Uint32 PixelData = *(Uint32*)pPixel;
+
+    SDL_Color Color = {0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE};
+
+    // Retrieve the RGB values of the specific pixel
+    SDL_GetRGB(PixelData, pSurface->format, &Color.r, &Color.g, &Color.b);
+
+    return Color;
+}
+
+void TestMatriceGuide(){
+    
+    SDL_Surface* pSurface = IMG_Load("src/img/t.png");
+    int w, h, x, y;
+    SDL_Color Color;
+
+    SDL_LockSurface(pSurface);
+    w = pSurface->w;
+    h = pSurface->h;
+
+    for(x=0;x<w;x++){
+        for(y=0;y<h;y++){
+            // Offset (x, y) in the image/surface
+            Color = GetPixelColor(pSurface, x, y);
+
+            
+            printf("The pixel at (%i,%i) has r = %i, g = %i, b = %i \n",x,y,Color.r,Color.g,Color.b);
+                
+        }
+    }
+
+    SDL_UnlockSurface(pSurface);
+    SDL_FreeSurface(pSurface);
+
+
+}
+
+void ImgToMatrix_1(int hauteur, int largeur, char nom_img[60]){
+    
+    SDL_Surface *pImg = IMG_Load(nom_img);
+    SDL_Color Color;
+    int mat[hauteur][largeur];
+    int i,j,w,h;
+    int r,g,b;
+
+    SDL_LockSurface(pImg);
+    w = pImg->w;
+    h = pImg->h;
+
+    //Création matrice
+    for(i=0;i<w;i++){
+        for(j=0;j<h;j++){
+            
+            // Offset (x, y) in the image/surface
+            Color = GetPixelColor(pImg, i, j);
+            
+            r = Color.r; g = Color.g; b = Color.b;
+
+            if((r == 0) && (g == 0) && (b == 0)){
+                mat[j][i]=0;
+            }
+            else if((r == 255) && (g == 255) && (b == 255)){
+                mat[j][i]=1;
+            }
+            else if((r == 255) && (g == 0) && (b == 0)){
+                mat[j][i]=2;
+            }
+            else{
+                mat[j][i]=-1;
+            }
+
+                
+        }
+    }
+
+    //Affichage matrice
+    for(i=0;i<h;i++){
+        printf("| ");
+        for(j=0;j<w;j++){
+            printf(" %i ",mat[i][j]);
+        }
+        printf(" |\n");
+    }
+
+    SDL_UnlockSurface(pImg);
+    SDL_FreeSurface(pImg);
+
+
+}
+
+void TestImgToMatrix(){
+    ImgToMatrix_1(10,10,"src/img/t.png");
+}
+
+void ImgToMatrix_2(SDL_Surface *pSurface, int mat[pSurface->h][pSurface->w]){
+    
+    SDL_Color Color;
+    int i,j,w,h;
+    int r,g,b;
+
+    w = pSurface->w;
+    h = pSurface->h;
+
+    //Création matrice
+    for(i=0;i<w;i++){
+        for(j=0;j<h;j++){
+            
+            // Offset (x, y) in the image/surface
+            Color = GetPixelColor(pSurface, i, j);
+            
+            r = Color.r; g = Color.g; b = Color.b;
+
+            if((r == 0) && (g == 0) && (b == 0)){
+                mat[j][i]=0;
+            }
+            else if((r == 255) && (g == 255) && (b == 255)){
+                mat[j][i]=1;
+            }
+            else if((r == 255) && (g == 0) && (b == 0)){
+                mat[j][i]=2;
+            }
+            else{
+                mat[j][i]=-1;
+            }
+
+                
+        }
+    }
+
+
+}
+
+void TestImgToMatrixVariable(){
+
+    char nom_img[60];
+    int w,h;
+    int i,j;
+
+    printf("Entrez l'emplacement du fichier image à traduire en matrice : ");
+    scanf("%s",nom_img);
+
+    SDL_Surface *pImg = IMG_Load(nom_img);
+    SDL_LockSurface(pImg);
+    w = pImg->w;
+    h = pImg->h;
+
+    int mat[h][w];
+
+    ImgToMatrix_2(pImg,mat);
+
+    //Affichage matrice
+    for(i=0;i<h;i++){
+        printf("| ");
+        for(j=0;j<w;j++){
+            printf(" %i ",mat[i][j]);
+        }
+        printf(" |\n");
+    }
+
+    SDL_UnlockSurface(pImg);
+    SDL_FreeSurface(pImg);
+
 
 
 }
 
 int main(int argc, char** argv){
 
+    
+    //Initialisations
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
     int action;
 
-    printf("Quelle action voulez-vous faire ?\n1 - Affichage SDL\n2 - .png to matrix\n");
+    printf("Quelle action voulez-vous faire ?\n1 - Affichage SDL\n2 -  test raté de .png to matrix\n3 - .png to color version copiée\n4 - .png to matrix\n5 - .png to matrix avec taille variable\n");
     scanf("%i",&action);
 
     switch(action){
         case 1:TestAffichage();break;
         case 2:TestMatrice();break;
+        case 3:TestMatriceGuide();break;
+        case 4:TestImgToMatrix();break;
+        case 5:TestImgToMatrixVariable();break;
         default : printf("Erreur pas d'action correspondante");break;
     }
+
+    IMG_Quit();
+    SDL_Quit();
 
     return(0);
 }
